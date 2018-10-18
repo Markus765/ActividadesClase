48b551c040f51cb4b0d2909a26954b600bc5aacf
Carlos <profesorderedes@gmail.com>
2018-10-18 15:58:24 +0200

    jue oct 18 15:58:24 CEST 2018

---
+++ Tema2/src/bucles/Ejercicio04.java
@@ -0,0 +1,32 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		int numero;
+
+		System.out.println("Dame un numero entre el 1 y el 5: ");
+		numero = entrada.nextInt();
+
+		while (numero != 3) {
+			if (numero < 1 || numero > 5) {
+				System.out.println("Se esperaba un número entre 1 y 5");
+			} else {
+				System.out.println("Incorrecto, otro numero entre el 1 y el 5: ");
+			}
+
+			numero = entrada.nextInt();
+		}
+
+		System.out.println("has acertado");
+
+		entrada.close();
+
+	}
+
+}
